ccd10f02bc1c7a54e027e1f9907f95bed256afc5
Mirabellensaft <tanja@transfeld.net>
2017-04-05 11:34:23 +0200

    pHcalculator

--- pHcalculator.py
+++ pHcalculator.py
@@ -1,17 +1,21 @@
-print "Enter concentration of acid in [mol/l]"
-cAc= float(raw_input())
+print "Enter concentration of the base in [mol/l]"
+cAc = float(raw_input())
 print "%r mol/l" % cAc
 
-print "Enter used Volume of acid in [l]"
+print "Enter used Volume of the base in [l]"
 vAc = float(raw_input())
 print "%r l" % vAc
 
-print "Enter Volume of base in [l]"
+print "Enter Volume of the acid in [l]"
 vBa = float(raw_input())
 print "%r l" % vBa
 
-print "Calculating the amount of substance [mol] of the acid"
+print "Calculating the amount of substance [mol] of the base"
 print vAc * cAc
 
-print "Calculating the concentration of the Base"
+print "Calculating the concentration of the acid"
 print (vAc * cAc) / vBa
+
+print "pH of the acid"
+import math
+print - math.log10((vAc * cAc) / vBa)
