29dcc266dfa7be757dfbd030af6d92a2d263fcc3
Mirabellensaft <tanja@transfeld.net>
2017-04-07 17:05:21 +0200

    added file output

--- pHcalculator.py
+++ pHcalculator.py
@@ -1,21 +1,57 @@
-print "Enter concentration of the base in [mol/l]"
-cAc = float(raw_input())
-print "%r mol/l" % cAc
+# import locale
+# locale.setlocale(locale.LC_ALL, 'de_DE')
+# print "{0:n}".format(11111.2111)
+print "Name the file your values will be saved in"
+filename = raw_input(">")
+target = open(filename, "w")
 
-print "Enter used Volume of the base in [l]"
-vAc = float(raw_input())
-print "%r l" % vAc
+name_base = raw_input("Enter name of base: ")
+conc_base = float(raw_input("Enter concentration of the base in [mol/l]"))
+print "%r mol/l" % conc_base
+vol_base = float(raw_input("Enter used Volume of the base in [l]"))
+print "%r l" % vol_base
+target.write("Substance: %r" % name_base)
+target.write("\n")
+target.write("Concentration: %d mol/l" % conc_base)
+target.write("\n")
+target.write("Volume used: %d l" % vol_base)
+target.write("\n")
 
-print "Enter Volume of the acid in [l]"
-vBa = float(raw_input())
-print "%r l" % vBa
+name_acid = raw_input("Enter name of acid: ")
+vol_acid = float(raw_input("Enter Volume of the acid in [l]"))
+print "%r l" % vol_base
+
+target.write("Substance: %r" % name_acid)
+target.write("\n")
+target.write("Volume: %d l" % vol_acid)
+target.write("\n")
 
 print "Calculating the amount of substance [mol] of the base"
-print vAc * cAc
+print vol_acid * conc_base
+mol_acid = vol_acid * conc_base
+
+target.write("Amount of substance of the acid: %d mol " % mol_acid)
+target.write("\n")
 
 print "Calculating the concentration of the acid"
-print (vAc * cAc) / vBa
+print (vol_base * conc_base) / vol_acid
+conc_acid = (vol_base * conc_base) / vol_acid
+
+target.write("Concentration of the acid: %d mol/l" % conc_acid)
+target.write("\n")
 
 print "pH of the acid"
 import math
-print - math.log10((vAc * cAc) / vBa)
+print - math.log10((vol_base * conc_base) / vol_acid)
+pH_acid = - math.log10((vol_base * conc_base) / vol_acid)
+
+target.write("pH of the acid: %d" % pH_acid)
+target.write("\n")
+
+target.close()
+
+
+# things to add:
+# auswahl saure base ehrprotonige sauren basen
+# ausgabe in datei schreiben, neue rechnung, neue zeile,
+# txt datei so schreiben, dass sie in exel importierbar ist
