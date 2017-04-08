35b52241ea4bb764f04bf1e4337ca69474a6796c
Mirabellensaft <tanja@transfeld.net>
2017-04-08 09:22:59 +0200

    code is prettier now

--- pHcalculator.py
+++ pHcalculator.py
@@ -1,8 +1,9 @@
 # import locale
 # locale.setlocale(locale.LC_ALL, 'de_DE')
 # print "{0:n}".format(11111.2111)
-print "Name the file your values will be saved in"
-filename = raw_input(">")
+import math
+
+filename = raw_input("Name the file your values will be saved in:")
 target = open(filename, "w")
 
 name_base = raw_input("Enter name of base: ")
@@ -10,43 +11,32 @@ conc_base = float(raw_input("Enter concentration of the base in [mol/l]"))
 print "%r mol/l" % conc_base
 vol_base = float(raw_input("Enter used Volume of the base in [l]"))
 print "%r l" % vol_base
-target.write("Substance: %r" % name_base)
-target.write("\n")
-target.write("Concentration: %d mol/l" % conc_base)
-target.write("\n")
-target.write("Volume used: %d l" % vol_base)
-target.write("\n")
+target.write("Substance: %r\n" % name_base)
+target.write("Concentration: %s mol/l\n" % conc_base)
+target.write("Volume used: %s l\n" % vol_base)
 
 name_acid = raw_input("Enter name of acid: ")
 vol_acid = float(raw_input("Enter Volume of the acid in [l]"))
 print "%r l" % vol_base
 
-target.write("Substance: %r" % name_acid)
-target.write("\n")
-target.write("Volume: %d l" % vol_acid)
-target.write("\n")
+target.write("Substance: %r\n" % name_acid)
+target.write("Volume: %s l\n" % vol_acid)
 
 print "Calculating the amount of substance [mol] of the base"
 print vol_acid * conc_base
-mol_acid = vol_acid * conc_base
+mol_base = vol_acid * conc_base
+print "Amount of substance of the base: %s mol." % mol_base
 
-target.write("Amount of substance of the acid: %d mol " % mol_acid)
-target.write("\n")
+target.write("Amount of substance of the acid: %s mol\n " % mol_acid)
 
-print "Calculating the concentration of the acid"
-print (vol_base * conc_base) / vol_acid
-conc_acid = (vol_base * conc_base) / vol_acid
+conc_acid = mol_base / vol_acid
+print "Concentration of the acid: %s mol/l" % conc_acid
 
-target.write("Concentration of the acid: %d mol/l" % conc_acid)
-target.write("\n")
+target.write("Concentration of the acid: %s mol/l\n" % conc_acid)
 
-print "pH of the acid"
-import math
-print - math.log10((vol_base * conc_base) / vol_acid)
-pH_acid = - math.log10((vol_base * conc_base) / vol_acid)
-
-target.write("pH of the acid: %d" % pH_acid)
-target.write("\n")
+pH_acid = - math.log10(mol_base / vol_acid)
+print "pH of the acid: %s" % pH_acid
+target.write("pH of the acid: %s\n" % pH_acid)
 
 target.close()
 
